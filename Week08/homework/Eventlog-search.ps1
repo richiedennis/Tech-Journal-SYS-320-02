5c77a6c38a83cb5bb8ea39cff96c68ecae78077e
richie dennis <richard.dennis@mymail.champlain.edu>
2021-10-23 20:39:31 -0700

    Week 8 Completed Assignments

---
+++ Week08/homework/Eventlog-search.ps1
@@ -0,0 +1,20 @@
+﻿# Storyline: Review the Security Event Log
+
+#Directory to save files:
+
+$myDir = "C:\Users\champuser\Desktop\"
+
+# List all the available Windows Event Logs
+Get-EventLog -list
+
+# Create a prompt to allow user to select the Log to view
+$readLog = Read-host -Prompt "Please select a log to review from the list above."
+
+# Print the results for the log
+
+# Task: Create a prompt that allows the user to specify a keyword or phrase to search on
+# Find a string from your event logs to search on
+$searchMessage = Read-host -Prompt "What message would you like to search the logs for?"
+
+Get-EventLog -LogName $readLog -Newest 40 | where {$_.Message -ilike "*$searchMessage*" } 
+#| export-csv -NoTypeInformation -Path "$myDir\securityLogs.csv"
---
+++ Week08/homework/Send-mail.ps1
@@ -0,0 +1,18 @@
+﻿# Storyline: Send an email
+
+# Body of the email
+
+# Variable can have an underscore or any alphanumeric value
+$msg = "Hello There."
+
+#echoing to the screen
+write-host -BackgroundColor Red -ForegroundColor White $msg
+
+# Email from address
+$email = "richard.dennis@mymail.champlain.edu"
+
+# To address
+$toemail = "deployer@csi-web"
+
+# Send the email
+Send-MailMessage -From $email -To $toemail -Subject "A Greeting" -Body $msg -SmtpServer 192.168.6.71
